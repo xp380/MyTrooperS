7c2094fa20e9506e7b335c6ab88235f066c00ad5
Vincent Kouoï <vincentkouoi@gmail.com>
2022-02-25 11:10:32 +0100

    search done

--- front/src/Pages/Search/index.js
+++ front/src/Pages/Search/index.js
@@ -1,8 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Search.css";
 
 const Search = () => {
-  return <div> Mes recherches</div>;
+  const [APIData, setAPIData] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
+
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      const filteredData = APIData.filter((item) => {
+        return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(APIData);
+    }
+  };
+
+  useEffect(() => {
+    axios.get(`http://localhost:8081/todos`).then((response) => {
+      setAPIData(response.data);
+    });
+  }, []);
+  console.log(APIData);
+
+  return (
+    <div style={{ marginTop: 20 }}>
+      <input
+        icon="search"
+        placeholder="Search..."
+        onChange={(e) => searchItems(e.target.value)}
+      />
+      {searchInput.length > 1
+        ? filteredResults.map((item) => {
+            return (
+              <div>
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+              </div>
+            );
+          })
+        : APIData.map((item) => {
+            return (
+              <div>
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+              </div>
+            );
+          })}
+    </div>
+  );
 };
 
 export default Search;
